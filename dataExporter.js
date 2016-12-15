fbd747f5d5ce723a7c8af785a40f34bb0cd58c08
Toby Fee <toby.fee@nwea.org>
2016-12-14 16:44:00 -0800

    smoke test

--- dataExporter.js
+++ dataExporter.js
@@ -7,53 +7,58 @@ var apiKey = process.env.SHOPIFY_API_KEY,
   s3Bucket = process.env.S3_BUCKET || 'demotobyfee',
   requestUrlBase = 'https://'+apiKey+':'+apiPassword+'@budboxco.myshopify.com/admin/'
 
-// request(requestUrlBase+'orders.json?status=any', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     fs.writeFile('datadump/orderDump.json', body, function (err) {
-//       if (err) return console.log(err);
-//       console.log('data has been dumped to OrderDump.json');
-//     });
-//   }
-// });
-//
-// request(requestUrlBase+'customers/count.json', function (error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     var customerCount = (JSON.parse(body)).count
-//     if (customerCount < 250){
-//       request(requestUrlBase+'customers.json?fields=email&limit=250', function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//               fs.writeFile('datadump/customerDump.json', body, function (err) {
-//                 if (err) return console.log(err);
-//                 console.log('Customer data has been dumped to customerDump.json');
-//               });
-//           var customerDump = (JSON.parse(body))
-//           fs.writeFile('datadump/emailDump.txt', '', function (err) {
-//             if (err) return console.log(err);
-//           });
-//           customerDump.customers.forEach(function(customerRecord){
-//             fs.appendFile('datadump/emailDump.txt',customerRecord.email+',', function(err){
-//               if (err) return console.log(err);
-//             });
-//           })
-//
-//         }
-//       })
-//     } else {
-//       console.log("Hey Toby you gotta update this thing, it can't all be done in one request. Total customer count is: "+customerCount)
-//     }
-//   }
-// });
-var s3bucket = new AWS.S3({params: {Bucket: s3Bucket}});
-fs.readFile('package.json', function(err, data){
-  var params = {
-    Key: 'shopify-dump/testText.txt',
-    Body: data
+request(requestUrlBase+'orders.json?status=any', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    fs.writeFile('datadump/orderDump.json', body, function (err) {
+      if (err) return console.log(err);
+      console.log('data has been dumped to orderDump.json');
+      uploadFile('orderDump.json');
+    });
   }
-  s3bucket.upload(params,function(err, data){
-    if (err) {
-        console.log('ERROR MSG: ', err);
+});
+
+request(requestUrlBase+'customers/count.json', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var customerCount = (JSON.parse(body)).count
+    if (customerCount < 250){
+      request(requestUrlBase+'customers.json?fields=email&limit=250', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+              fs.writeFile('datadump/customerDump.json', body, function (err) {
+                if (err) return console.log(err);
+                console.log('Customer data has been dumped to customerDump.json');
+                uploadFile('customerDump.json');
+              });
+          var customerDump = (JSON.parse(body))
+          fs.writeFile('datadump/emailDump.txt', '', function (err) {
+            if (err) return console.log(err);
+          });
+          customerDump.customers.forEach(function(customerRecord){
+            fs.appendFile('datadump/emailDump.txt',customerRecord.email+',', function(err){
+              if (err) return console.log(err);
+            });
+          });
+          uploadFile('emailDump.txt');
+
+        }
+      })
     } else {
-        console.log('Successfully uploaded data' + data);
+      console.log("Hey Toby you gotta update this thing, it can't all be done in one request. Total customer count is: "+customerCount)
     }
+  }
+});
+var s3bucket = new AWS.S3({params: {Bucket: s3Bucket}});
+function uploadFile(filename){
+  fs.readFile('datadump/'+filename, function(err, data){
+    var params = {
+      Key: Date.now()+filename,
+      Body: data
+    }
+    s3bucket.upload(params,function(err, data){
+      if (err) {
+          console.log('ERROR MSG: ', err);
+      } else {
+          console.log('Successfully uploaded data for file ' + filename);
+      }
+    })
   })
-})
+}
